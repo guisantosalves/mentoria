6e2b90e218b31428abdcadd6e62603d671a86706
guisantosalves <workguisantos@gmail.com>
2024-11-13 20:46:39 -0300

    setting databse

---
+++ backend/source/db/migrations/20241113230025_user_entity/migration.sql
@@ -0,0 +1,15 @@
+-- CreateTable
+CREATE TABLE "Usuario" (
+    "id" SERIAL NOT NULL,
+    "nome" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "senha" TEXT NOT NULL,
+    "tipo" INTEGER NOT NULL DEFAULT 0,
+    "foto" TEXT NOT NULL,
+    "RG" TEXT NOT NULL,
+    "CPF" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Usuario_pkey" PRIMARY KEY ("id")
+);
