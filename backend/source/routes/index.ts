206d788f9b6c1bfe1c261da2eee5b1bb2c9c4857
guisantosalves <workguisantos@gmail.com>
2024-11-30 20:57:17 -0300

    inserting auth

--- backend/source/routes/index.ts
+++ backend/source/routes/index.ts
@@ -18,53 +18,60 @@ router.post("/login", LoginController.logging);
 // usuario
 router.get("/usuario", auth, UserController.getUser);
 router.get("/usuario/:id", auth, UserController.getUserById);
-router.post("/usuario", auth, UserController.createUser);
+// create user does not need auth
+router.post("/usuario", UserController.createUser);
 router.put("/usuario/:id", auth, UserController.updateUser);
 router.delete("/usuario/:id", auth, UserController.deleteUser);
 
 // departamento
-router.get("/departamento", DepController.getDepartamento);
-router.get("/departamento/:id", DepController.getDepById);
-router.post("/departamento", DepController.createDep);
-router.put("/departamento/:id", DepController.updateDep);
-router.delete("/departamento/:id", DepController.deleteDep);
+router.get("/departamento", auth, DepController.getDepartamento);
+router.get("/departamento/:id", auth, DepController.getDepById);
+router.post("/departamento", auth, DepController.createDep);
+router.put("/departamento/:id", auth, DepController.updateDep);
+router.delete("/departamento/:id", auth, DepController.deleteDep);
 
 // avaliacao
-router.get("/avaliacao", AvaliacaoController.getAvaliacao);
-router.get("/avaliacao/:id", AvaliacaoController.getAvaliacaoById);
+router.get("/avaliacao", auth, AvaliacaoController.getAvaliacao);
+router.get("/avaliacao/:id", auth, AvaliacaoController.getAvaliacaoById);
 router.get(
   "/avaliacao/mentoria/:mentoriaId",
+  auth,
   AvaliacaoController.getAvaliacaoByMentoria
 );
-router.post("/avaliacao", AvaliacaoController.createAvaliacao);
-router.put("/avaliacao/:id", AvaliacaoController.updateAvaliacao);
-router.delete("/avaliacao/:id", AvaliacaoController.deleteAvaliacao);
+router.post("/avaliacao", auth, AvaliacaoController.createAvaliacao);
+router.put("/avaliacao/:id", auth, AvaliacaoController.updateAvaliacao);
+router.delete("/avaliacao/:id", auth, AvaliacaoController.deleteAvaliacao);
 
 // curso
-router.get("/curso", CursoController.getCurso);
-router.get("/curso/disciplina", CursoController.getCursoAndDisciplinas);
-router.get("/curso/:id", CursoController.getCursoById);
-router.post("/curso", CursoController.createCurso);
-router.put("/curso/:id", CursoController.updateCurso);
-router.delete("/curso/:id", CursoController.deleteCurso);
+router.get("/curso", auth, CursoController.getCurso);
+router.get("/curso/disciplina", auth, CursoController.getCursoAndDisciplinas);
+router.get("/curso/:id", auth, CursoController.getCursoById);
+router.post("/curso", auth, CursoController.createCurso);
+router.put("/curso/:id", auth, CursoController.updateCurso);
+router.delete("/curso/:id", auth, CursoController.deleteCurso);
 
 // disciplina
-router.get("/disciplina", DisciplinaController.getDisciplina);
+router.get("/disciplina", auth, DisciplinaController.getDisciplina);
 router.get(
   "/disciplina/mentoria",
+  auth,
   DisciplinaController.getDisciplinaAndMentorias
 );
-router.get("/disciplina/:id", DisciplinaController.getDisciplinaById);
-router.post("/disciplina", DisciplinaController.createDisciplina);
-router.put("/disciplina/:id", DisciplinaController.updateDisciplina);
-router.delete("/disciplina/:id", DisciplinaController.deleteDisciplina);
+router.get("/disciplina/:id", auth, DisciplinaController.getDisciplinaById);
+router.post("/disciplina", auth, DisciplinaController.createDisciplina);
+router.put("/disciplina/:id", auth, DisciplinaController.updateDisciplina);
+router.delete("/disciplina/:id", auth, DisciplinaController.deleteDisciplina);
 
 // mentoria
-router.get("/mentoria", MentoriaController.getMentoria);
-router.get("/mentoria/:id", MentoriaController.getMentoriaById);
-router.get("/mentoria/:id/details", MentoriaController.getMentoriaByIdComplete);
-router.post("/mentoria", MentoriaController.createMentoria);
-router.put("/mentoria/:id", MentoriaController.updateMentoria);
-router.delete("/mentoria/:id", MentoriaController.deleteMentoria);
+router.get("/mentoria", auth, MentoriaController.getMentoria);
+router.get("/mentoria/:id", auth, MentoriaController.getMentoriaById);
+router.get(
+  "/mentoria/:id/details",
+  auth,
+  MentoriaController.getMentoriaByIdComplete
+);
+router.post("/mentoria", auth, MentoriaController.createMentoria);
+router.put("/mentoria/:id", auth, MentoriaController.updateMentoria);
+router.delete("/mentoria/:id", auth, MentoriaController.deleteMentoria);
 
 export default router;
